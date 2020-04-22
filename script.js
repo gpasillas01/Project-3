ab56b59146573709d215eb9059f1a026c3a36442
gpasillas01 <60024567+gpasillas01@users.noreply.github.com>
2020-04-21 21:56:18 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function initMap() {
-  var iit_tower = {lat: 41.831602, lng: -87.627231};
-  var map = new google.maps.Map(document.getElementById('map'), {
-		  zoom: 18, 
-		  center: iit_tower,
-	      mapTypeId: 'satellite'
-	  });
-  var marker = new google.maps.Marker({position: iit_tower, map: map});
-}
