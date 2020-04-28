9bcebffb9c6ec8f158c43eeea31999516a4e79d4
gpasillas01 <60024567+gpasillas01@users.noreply.github.com>
2020-04-28 14:23:49 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -27,21 +27,13 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
-var map;
-function initMap()
-{
-var latlang={lat:4.710859,lng:-74.074955};
-mapa=new google.maps.Map(
-		document.getElementById('gmaps'),
-		{center: new google.maps.LatLng(41.835668, -87.624221), zoom:15});
-
-var marker= new google.maps.Marker({position:latlang,map:map});
-
-marker= new google.maps.Marker({
-	map:map,
-	draggable:true,
-	animation:google.maps.Animation.BOUNCE,
-	position:new google.maps.LatLng(6.219131, -75.566329),
-});
-}
-
+//initialize map//
+function initMap() {
+ // The location of Uluru
+ var uluru = {lat:41.834433,lng: -87.627020};
+ // The map, centered at Uluru
+ var map = new google.maps.Map(
+     document.getElementById('map'), {zoom: 4, center: uluru});
+ // The marker, positioned at Uluru
+ var marker = new google.maps.Marker({position: uluru, map: map});
+	window.onload = changeImg;
