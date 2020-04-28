ba2a8569b3afc1818d2a14f1f11bda3d0aefd0fe
gpasillas01 <60024567+gpasillas01@users.noreply.github.com>
2020-04-28 14:20:26 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -27,25 +27,21 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
-// Initialize and adding  map//
-
 var map;
 function initMap()
 {
-    // The location of//
 var latlang={lat:4.710859,lng:-74.074955};
 mapa=new google.maps.Map(
 		document.getElementById('gmaps'),
-		{center: new google.maps.LatLng(41.835812, -87.625853), zoom:15});
-//^map centered mtcc//
+		{center: new google.maps.LatLng(41.835668, -87.624221), zoom:15});
+
 var marker= new google.maps.Marker({position:latlang,map:map});
-//movement control w marker//
+
 marker= new google.maps.Marker({
 	map:map,
 	draggable:true,
 	animation:google.maps.Animation.BOUNCE,
-	position:new google.maps.LatLng(8.2, -75.90),
+	position:new google.maps.LatLng(6.219131, -75.566329),
 });
 }
-
 
