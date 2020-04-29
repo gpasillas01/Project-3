d58af0a5fe9d2403ec74988762a40299c09ec7b3
gpasillas01 <60024567+gpasillas01@users.noreply.github.com>
2020-04-28 19:30:03 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -26,14 +26,90 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
-
+//*
 // Initialize and add the map//
 function initMap() {
-  // The location of Uluru//
-  var uluru = {lat:41.834433,lng: -87.627020};
-  // The map, centered at Uluru//
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
-  // The marker, positioned at Uluru//
-  var marker = new google.maps.Marker({position: uluru, map: map});
+   // map option //
+  var options = {
+   zoom: 8,
+   center:{lat:41.834433,lng: -87.627020}
+  }
+
+  //new map//
+  var map = new
+  google.maps.Map (document.getElementById('map'), options);
+  
+  //Listen for map clicks 
+  google.maps.event.addListener(map,'click',
+   function(event){
+    //adding random location pins when clicked
+    addMarker({coords:event.lating});
+  });
+  
+  /*
+  //marker//
+  var marker = new google.maps.Marker({
+    position:{lat:41.834815,lng: -87.626904},
+    map:map
+    icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+  }};
+ var infoWindow = new google.maps.InfoWindow({
+     content:'<h1>Chicago, IL</h1>'});
+  //listener//
+  marker.addListener('click',function(){
+    infoWindow.open(map, marker);
+  });
+  */
+  //Array maarkers
+  vars markers=[
+    { 
+    coords:{lat:41.834433,lng: -87.627020},
+    iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+    content:'<h1>Chicago</h1>'
+  },
+    {
+  coords:{lat:41.882697,lng: -87.624205},
+    },
+      {    
+  coords:{lat:39.735088,lng: -104.959735},
+      },
+    {
+     coords:{29.766089, -95.367785}
+    }
+   ];
+  
+  
+  //loop through markers
+  for(var i=0;i<markers.length;i++){
+    //add marker
+    addMarker(markers[i];
+              }
+  
+  
+  //add marker function
+  function addMarker(coords){
+   var marker = new google.maps.Marker({
+    position:{coords},
+    map:map
+    //icon:props.iconImage
+     
+  }};
+   //check custom.                                    
+  if(props.iconImage){
+     //icon image
+     marker.setIcon(props.iconImage);
 }
+    //check content
+    if(props.content){
+      var infoWindow = new google.maps.InfoWindow({
+     content:props.content 
+      
+      });
+  //listener//
+  marker.addListener('click',function(){
+    infoWindow.open(map, marker);
+  });
+  }
+}
+                                      
+                                      
